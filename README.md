4d2ca6dfcf157cb378bca85099de66f911ec4294
Toni Powell <tonipow3ll@gmail.com>
2021-01-07 10:33:31 -0500

    initial commit, basic styling and pages set up

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Code-Quiz
+timed code quiz
---
+++ script.js
@@ -0,0 +1,22 @@
+// getelement by ID for start and next button, only one should show at a time 
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+// need "start button" to show on initial page load
+// need "next" button to show once question has been 'answered'
+
+// #question - insert 'questions' into text area
+// #answer-buttons - insert 'answers' into text areas
+// class for "timer" - need to set interval 
+
+// function to 'begin quiz', should also start timer
+function startQuiz()
+
+// function to bring up the 'next question' 
+function nextQuestion()
+
+
+// will need an event listener so page will NOT refresh when 'answer' is selected
+function selectAnswer()
+
+
+// will need questions / answers in an object 
